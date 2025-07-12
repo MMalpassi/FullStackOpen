ca438f0d30e53548186b0b96bd8b89eb9607344b
Mateo Malpassi <mateo.malpass@mi.unc.edu.ar>
2025-07-12 16:30:25 -0300

    part 3; exercises 3.1-3.6

---
+++ part3/phonebook/index.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const app = express()
+
+let phonebook = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/phonebook', (request, response) => {
+  response.json(phonebook)
+})
+
+app.get('/api/info', (request,response) => {
+  const hour = new Date();
+
+  response.send(`
+    <p>Phonebook has info for ${phonebook.length} people</p>
+    <p>${hour}</p>
+  `)
+})
+
+app.get('/api/phonebook/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const phonebook_contacts = phonebook.find(phonebook => phonebook.id === id)
+
+  if (phonebook_contacts) {
+    response.json(phonebook_contacts)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/phonebook/:id', (request, response) => {
+  const id = Number(request.params.id)
+  phonebook = phonebook.filter(phonebook_contact => phonebook_contact.id !== id)
+
+  response.status(204).end()
+})
+
+app.use(express.json())
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000);
+};
+
+app.post('/api/phonebook', (request, response) => {
+  const body = request.body
+
+  if (!body.number || !body.name) {
+    return response.status(400).json({ 
+      error: 'number or name missing' 
+    })
+  }
+
+  const nameRepeated = phonebook.some(phonebook_contact => phonebook_contact.name === body.name);
+  if (nameRepeated) {
+    return response.status(400).json({
+      error: 'El nombre ya existe en la agenda'
+    });
+  }
+
+  const phonebook_contact = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  phonebook = phonebook.concat(phonebook_contact)
+
+  response.json(phonebook)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})

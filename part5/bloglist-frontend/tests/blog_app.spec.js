4426f6de20d65e0b0026f48338c4b98216558cc7
Mateo Malpassi <mateo.malpass@mi.unc.edu.ar>
2025-09-12 17:03:36 -0300

    exercises 5.20-5.21

--- part5/bloglist-frontend/tests/blog_app.spec.js
+++ part5/bloglist-frontend/tests/blog_app.spec.js
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test.describe('Blog app', () => {
   test.beforeEach(async ({ page, request }) => {
+    // Resetear DB y crear usuario
     await request.post('http://localhost:3001/api/testing/reset')
     const newUser = {
       name: 'Tester',
@@ -12,59 +13,44 @@ test.describe('Blog app', () => {
     await page.goto('http://localhost:5173')
   })
 
-  test('Login form is shown', async ({ page }) => {
-    const heading = page.getByText('Login to application')
-    const usernameInput = page.getByLabel(/username/i)
-    const passwordInput = page.getByLabel(/password/i)
-    const submitButton = page.getByRole('button', { name: /login/i })
-
-    await expect(heading).toBeVisible()
-    await expect(usernameInput).toBeVisible()
-    await expect(passwordInput).toBeVisible()
-    await expect(submitButton).toBeVisible()
-  })
-
-  test.describe('Login', () => {
-    test('succeeds with correct credentials', async ({ page }) => {
-      await page.getByLabel(/username/i).fill('tester')
-      await page.getByLabel(/password/i).fill('123456')
-      await page.getByRole('button', { name: /login/i }).click()
-    })
-
-    test('fails with wrong credentials', async ({ page }) => {
-      await page.getByLabel(/username/i).fill('tester')
-      await page.getByLabel(/password/i).fill('wrongPassw')
-      await page.getByRole('button', { name: /login/i }).click()
-      await expect(page.getByText('Tester logged-in')).not.toBeVisible()
-    })
-  })
-
   test.describe('When logged in', () => {
-    test.beforeEach(async ({ page, request }) => {
-      await request.post('http://localhost:3001/api/testing/reset')
-      const newUser = {
-        name: 'Tester',
-        username: 'tester',
-        password: '123456'
-      }
-      await request.post('http://localhost:3001/api/users/', { data: newUser })
-      await page.goto('http://localhost:5173')
+    test('Login - Create blog - Liked (edited) - Delete', async ({ page }) => {
+      //Logged-in test
       await page.getByLabel(/username/i).fill('tester')
       await page.getByLabel(/password/i).fill('123456')
       await page.getByRole('button', { name: /login/i }).click()
-      await expect(page.getByText('Tester logged-in')).toBeVisible()
-    })
+      await expect(page.getByText(/Tester logged-in/)).toBeVisible()
 
-    test('a new blog can be created', async ({ page }) => {
+      //Create blog test
       const newBlogButton = page.getByRole('button', { name: /new blog/i })
-      await expect(newBlogButton).toBeVisible()
       await newBlogButton.click()
-
-      await page.getByLabel('Title:').fill('Test blog')
+      await page.getByLabel('Title:').fill('Blog to like')
       await page.getByLabel('Author:').fill('Tester')
       await page.getByLabel('URL:').fill('https://test.com')
       await page.getByRole('button', { name: /create/i }).click()
-      await expect(page.getByText('Test blog by Tester')).toBeVisible()
+
+      //Like blog test
+      const blogBasic = page.getByTestId('blog-basic')
+      await expect(blogBasic).toBeVisible()
+      await expect(blogBasic).toHaveText('Blog to like by Tester')
+
+      const viewButton = page.getByRole('button', { name: /view info about/i })
+      await viewButton.click()
+
+      const likesSpan = page.getByTestId('blog-likes')
+      const likeButton = page.getByRole('button', { name: /like/i })
+
+      await expect(likesSpan).toHaveText('0')
+      await likeButton.click()
+      await expect(likesSpan).toHaveText('1')
+      await likeButton.click()
+      await expect(likesSpan).toHaveText('2')
+
+      //Delete the blog
+      page.on('dialog', dialog => dialog.accept())
+      const deleteButton = page.getByRole('button', { name: /remove/i })
+      await deleteButton.click()
+      await expect(page.getByTestId('blog-basic')).not.toHaveText('Blog to delete by Tester')
     })
   })
 })
